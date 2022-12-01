7f0c0233c38e6aa742c41080a62cce4036cdeb86
Harriet Holman <harriet.holman@epigenesys.org.uk>
2022-12-01 19:11:36 +0000

    Adding comments to explain each step

--- day01/day01.rb
+++ day01/day01.rb
@@ -29,17 +29,23 @@ input = File.open("./input.txt").read.split("\n\n")
 #puts elf.food[1].class
 #puts elf.calories
 
+# Create an array for multiple elves
 elves = []
 
+# For each item in the input create a new elf and allocate its calories
 input.each_with_index do |food_as_string, index|
   elf = Elf.new(food_as_string, index, :calories)
+  #add the elf and its calories to the array
   elves.push(elf)
 end
 
+# Sort the array of elves by calories
 elves = elves.sort_by(&:calories)
 #puts "Elf #{elves[241]} has #{elves[241].calories} calories"
 
-top_elves = elves[239..241]
+# Create a new array of only the top three
+top_elves = elves.last(3)
+# For all of the elves in that array, add up the total calories
 top_elves_calories = top_elves.map(&:calories).sum
-puts top_elves
+# Spit out the answer
 puts "Top three elves have #{top_elves_calories} calories"
