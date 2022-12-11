6d5127d814dbe212d4629d8894e578726dca34bd
Harriet Holman <harriet.holman@epigenesys.org.uk>
2022-12-11 17:17:08 +0000

    Day 08 Update and Dad's thing

---
+++ dad.rb
@@ -0,0 +1,32 @@
+def say_hi
+  puts "Hi!"
+end
+
+say_hi
+
+
+
+def fishcakes
+
+  ryan = 3
+  mum = 2
+  dad = 8
+  harriet = 7
+
+  # Add up the total number of fishcakes everyone in the family has
+  # and print to the output "We have X fishcakes."
+  
+  puts "We have #{ryan + mum + dad + harriet} fishcakes"
+
+end
+
+fishcakes
+
+# Print out fishcakes 5 times
+
+pCount = 0
+
+until pCount == 5
+  puts fishcakes
+  pCount = pCount+1  
+end
